251608a49386384013c5f6c532ed6ec2f9d8b58c
anshjaat-ui <anshwalyan@gmail.com>
2026-07-13 05:41:12 -0700

    Enhance DB connection with MONGO_URI logging
    Added logging for MONGO_URI during database connection.

--- new backend/amazon-clone-backend/config/db.js
+++ new backend/amazon-clone-backend/config/db.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose'
 
 export default async function connectDB() {
   try {
+    console.log("MONGO_URI:", process.env.MONGO_URI) // 👈 yahan add karo
+
     const conn = await mongoose.connect(process.env.MONGO_URI)
+
     console.log(`MongoDB connected: ${conn.connection.host}`)
   } catch (err) {
     console.error(`MongoDB connection error: ${err.message}`)
