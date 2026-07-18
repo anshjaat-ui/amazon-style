35cb4527a73d377eb3cca25075acbab378ba52f0
anshjaat-ui <anshwalyan@gmail.com>
2026-07-18 01:54:31 -0700

    Add suggestions route to productRoutes

--- new backend/amazon-clone-backend/routes/productRoutes.js
+++ new backend/amazon-clone-backend/routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import {
   getProducts,
+  getSuggestions,
   getProductById,
   createProduct,
   updateProduct,
@@ -12,6 +13,7 @@ import { protect, admin } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 router.get('/', getProducts)
+router.get('/suggestions', getSuggestions)
 router.get('/:id', getProductById)
 router.post('/', protect, admin, createProduct)
 router.put('/:id', protect, admin, updateProduct)
