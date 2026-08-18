a22c9694fe1e949bd76b6b1964ce5adb0ade8825
anshjaat-ui <anshwalyan@gmail.com>
2026-08-17 17:58:37 -0700

    Add files via upload

---
+++ amazon-clone-backend/amazon-clone-backend/routes/authRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import { signup, login, getProfile, updateProfile, addAddress, getAllUsers } from '../controllers/authController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
+
+const router = express.Router()
+
+router.post('/signup', signup)
+router.post('/login', login)
+router.get('/profile', protect, getProfile)
+router.put('/profile', protect, updateProfile)
+router.put('/profile/address', protect, addAddress)
+router.get('/users', protect, admin, getAllUsers)
+
+export default router
