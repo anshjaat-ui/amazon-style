2225e136ce416cf895d9901cc0b8cea11e87e82d
anshjaat-ui <anshwalyan@gmail.com>
2026-08-18 05:45:49 -0700

    Delete amazon-clone-backend/amazon-clone-backend directory

--- amazon-clone-backend/amazon-clone-backend/routes/deliveryRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { checkDelivery } from '../controllers/deliveryController.js'
-
-const router = express.Router()
-
-router.post('/', checkDelivery)
-
-export default router
